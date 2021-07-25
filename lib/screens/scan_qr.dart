4c787a8bf7c894a0d1285b783d0cb8a313ee1065
Mugunth J C <jc.mugunth@gmail.com>
2021-07-25 16:08:04 +0530

    initial

--- lib/screens/scan_qr.dart
+++ lib/screens/scan_qr.dart
@@ -14,14 +14,15 @@ class _qrCodeScannerPageState extends State<qrCodeScannerPage> {
   Barcode? result;
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
+  void showSnackbar(BuildContext context, String filename) {
+    var snackBar = SnackBar(
+      shape: StadiumBorder(),
+      elevation: 20,
+      behavior: SnackBarBehavior.floating,
+      duration: Duration(seconds: 3),
+      content: Text('Copied to clipboard!'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
@@ -83,13 +84,10 @@ class _qrCodeScannerPageState extends State<qrCodeScannerPage> {
   }
 
   Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
             MediaQuery.of(context).size.height < 400)
         ? 150.0
         : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
     return QRView(
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
